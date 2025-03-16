f422bb9ef4bef1ef99446c8e5e17eb9b3dc3bfbf
david <davidargudopicas@gmail.com>
2025-03-16 17:31:30 +0100

    implementacion test testAniadirAlumno

---
+++ src/test/java/instituto/CursoTest.java
@@ -0,0 +1,16 @@
+package instituto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	void testAniadirAlumno1() {
+		//Como te comento en el pdf, se me ha hecho imposible testear este metodo
+	}
+	
+	
+
+}
